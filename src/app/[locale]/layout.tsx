cf2dbedf79b56ee01f33a3bf1cb76b6eb6e90c33
Kaua Villa Nova <kvn.contact.professional@gmail.com>
2026-03-20 23:12:38 -0300

    feature: add metadata for portfolio layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,12 +2,19 @@ import { Rajdhani } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { ScreenSizeProvider } from "@/providers";
 import "@/styles/globals.scss";
+import { Metadata } from "next";
 
 const rajdhani = Rajdhani({
   subsets: ["latin"],
   variable: "--font-rajdhani",
   weight: ["300", "400", "500", "600", "700"],
 });
+
+export const metadata: Metadata = {
+  title: "Kauã Villa Nova - Portfolio",
+  description:
+    "Welcome to my portfolio! I'm a passionate software developer with experience in web development, mobile apps, and more. Explore my projects and skills to see how I can contribute to your next venture.",
+};
 
 export default function RootLayout({
   children,
